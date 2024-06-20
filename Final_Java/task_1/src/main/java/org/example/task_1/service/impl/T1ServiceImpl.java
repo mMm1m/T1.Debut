824a266611babffecad5f194412feb342588fab4
Maxim Golchanskiy <golchanskiy.m.d@gmail.com>
2024-06-20 21:27:09 +0300

    Updated signUp() method with rediection

--- Final_Java/task_1/src/main/java/org/example/task_1/service/impl/T1ServiceImpl.java
+++ Final_Java/task_1/src/main/java/org/example/task_1/service/impl/T1ServiceImpl.java
@@ -1,6 +1,9 @@
 package org.example.task_1.service.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import feign.FeignException;
+import jdk.swing.interop.SwingInterOpUtils;
 import org.example.task_1.enums.ConstStrings;
 import org.example.task_1.model.User;
 import org.example.task_1.openFeignClient.T1Client;
@@ -9,24 +12,28 @@ import org.example.task_1.requestRecords.TokenRequest;
 import org.example.task_1.requestRecords.UserRequest;
 import org.example.task_1.service.T1Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Service
-public class T1ServiceImpl implements T1Service {
+public class T1ServiceImpl {
     @Autowired
     private T1Client client;
     @Autowired
     private UserRepository userRepository;
+
+    ObjectMapper objectMapper = new ObjectMapper();
 
     private String encodeBASE64String(String startCondition){
         return Base64.getEncoder().encodeToString(startCondition.getBytes(StandardCharsets.US_ASCII));
@@ -90,7 +97,24 @@ public class T1ServiceImpl implements T1Service {
         }
     }
 
-    public ResponseEntity<String> signUp(@RequestBody UserRequest user){
+    public ResponseEntity<String> signUp(@RequestBody UserRequest user) throws JsonProcessingException {
+        // URL для отправки запроса
+        RestTemplate restTemplate = new RestTemplate();
+        String url  = "http://193.19.100.32:7000/api/sign-up";
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+        System.out.println(user.toString());
+        HttpEntity<String> entity = new HttpEntity<>(objectMapper.writeValueAsString(user), headers);
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, entity, String.class);
+        Set<String> alreadyVisited = new HashSet<>();
+        // пусть максимум может быть 10 перенаправлений
+        while(response.getStatusCode().is3xxRedirection()){
+            String redirectUrl = response.getHeaders().getLocation().toString();
+            if(alreadyVisited.contains(redirectUrl) || alreadyVisited.size() > 10)
+                return new ResponseEntity<>("Взаимная блокировка", HttpStatus.TEMPORARY_REDIRECT);
+            alreadyVisited.add(redirectUrl);
+            response = restTemplate.exchange(redirectUrl, HttpMethod.POST, entity, String.class);
+        }
         if(!isValidEmail(user.email())) return new ResponseEntity<>("Некорректная почта пользователя", HttpStatus.BAD_REQUEST);
         boolean flag = false;
         for(var a : ConstStrings.values())
@@ -105,8 +129,7 @@ public class T1ServiceImpl implements T1Service {
         for(var a : all) {
             if(a.getEmail().equals(user.email())) return new ResponseEntity<>("Пользователь с такой почтой уже есть", HttpStatus.BAD_REQUEST);
         }
-        client.signUp(user);
         userRepository.save(new User(user.last_name(), user.first_name(), user.email(), user.role(), "", "", ""));
-        return new ResponseEntity<>("Данные внесены", HttpStatus.OK);
+        return new ResponseEntity<>("Данные внесены", response.getStatusCode());
     }
 }
